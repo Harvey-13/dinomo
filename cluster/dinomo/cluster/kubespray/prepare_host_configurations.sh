ec2cc340410df323a7e365e1cad79d163c74aaad
Harvey13 <1762094211@qq.com>
2022-12-10 15:28:40 +0800

    Update nodes

--- cluster/dinomo/cluster/kubespray/prepare_host_configurations.sh
+++ cluster/dinomo/cluster/kubespray/prepare_host_configurations.sh
@@ -28,9 +28,10 @@ PREPARE_IB_CONFIG() {
 #        sudo bash -c 'echo \"address 10.0.0.$1\" >> /etc/network/interfaces' && \
 #        sudo bash -c 'echo \"netmask 255.255.255.0\" >> /etc/network/interfaces' && \
 #        sudo bash -c 'echo \"broadcast 10.0.0.255\" >> /etc/network/interfaces' && \
-        retVal= "sudo /etc/init.d/openibd restart && \
-        sudo modprobe -a mlx4_core mlx4_ib mlx4_en mlx5_core mlx5_ib \
-        ib_umad ib_uverbs ib_ipoib rdma_cm rdma_ucm;"
+        retVal= ""
+#        "sudo /etc/init.d/openibd restart && \
+#        sudo modprobe -a mlx4_core mlx4_ib mlx4_en mlx5_core mlx5_ib \
+#        ib_umad ib_uverbs ib_ipoib rdma_cm rdma_ucm;"
 #        mlx5_fpga_tools ib_ucm
 }
 
